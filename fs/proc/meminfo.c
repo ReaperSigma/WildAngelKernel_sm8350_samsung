c74267e1dfac27e643c1aa402c4f0e685e56fef6
ReaperSigma <vidium400@gmail.com>
2023-01-08 18:29:30 +0100

    Fix fs/proc/meminfo.c

--- fs/proc/meminfo.c
+++ fs/proc/meminfo.c
@@ -6,7 +6,6 @@
 #include <linux/hugetlb.h>
 #include <linux/mman.h>
 #include <linux/mmzone.h>
-#include <linux/memory.h>
 #include <linux/proc_fs.h>
 #include <linux/percpu.h>
 #include <linux/seq_file.h>
@@ -17,42 +16,21 @@
 #ifdef CONFIG_CMA
 #include <linux/cma.h>
 #endif
+#ifdef CONFIG_HUGEPAGE_POOL
+#include <linux/hugepage_pool.h>
+#endif
 #include <asm/page.h>
 #include <asm/pgtable.h>
 #include "internal.h"
-#ifdef CONFIG_QCOM_MINIDUMP_PANIC_DUMP
-#include <soc/qcom/minidump.h>
-#include <linux/seq_buf.h>
-#endif
 
 void __attribute__((weak)) arch_report_meminfo(struct seq_file *m)
 {
 }
 
-static void show_val_kb2(struct seq_file *m, const char *s, unsigned long num)
-{
-	if (m) {
-		seq_printf(m, s, num);
-	} else {
-#ifdef CONFIG_QCOM_MINIDUMP_PANIC_DUMP
-		if (md_meminfo_seq_buf)
-			seq_buf_printf(md_meminfo_seq_buf, s, num);
-#endif
-	}
-}
-
 static void show_val_kb(struct seq_file *m, const char *s, unsigned long num)
 {
-	if (m) {
-		seq_put_decimal_ull_width(m, s, num << (PAGE_SHIFT - 10), 8);
-		seq_write(m, " kB\n", 4);
-	} else {
-#ifdef CONFIG_QCOM_MINIDUMP_PANIC_DUMP
-		if (md_meminfo_seq_buf)
-			seq_buf_printf(md_meminfo_seq_buf, "%s : %lld KB\n", s,
-					num << (PAGE_SHIFT - 10));
-#endif
-	}
+	seq_put_decimal_ull_width(m, s, num << (PAGE_SHIFT - 10), 8);
+	seq_write(m, " kB\n", 4);
 }
 
 static int meminfo_proc_show(struct seq_file *m, void *v)
@@ -64,6 +42,8 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 	unsigned long pages[NR_LRU_LISTS];
 	unsigned long sreclaimable, sunreclaim;
 	int lru;
+	long rbinfree;
+	long hugepage_pool_pages = 0;
 
 	si_meminfo(&i);
 	si_swapinfo(&i);
@@ -71,6 +51,13 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 
 	cached = global_node_page_state(NR_FILE_PAGES) -
 			total_swapcache_pages() - i.bufferram;
+	rbinfree = atomic_read(&rbin_free_pages);
+#ifdef CONFIG_HUGEPAGE_POOL
+	hugepage_pool_pages = total_hugepage_pool_pages();
+#endif
+#ifdef CONFIG_ION_RBIN_HEAP
+	cached += atomic_read(&rbin_cached_pages);
+#endif
 	if (cached < 0)
 		cached = 0;
 
@@ -78,17 +65,12 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 		pages[lru] = global_node_page_state(NR_LRU_BASE + lru);
 
 	available = si_mem_available();
-
-#ifdef CONFIG_QCOM_MEM_OFFLINE
-	i.totalram = get_totalram_pages_count_inc_offlined();
-#endif
-
 	sreclaimable = global_node_page_state(NR_SLAB_RECLAIMABLE);
 	sunreclaim = global_node_page_state(NR_SLAB_UNRECLAIMABLE);
 
 	show_val_kb(m, "MemTotal:       ", i.totalram);
-	show_val_kb(m, "MemFree:        ", i.freeram);
-	show_val_kb(m, "MemAvailable:   ", available);
+	show_val_kb(m, "MemFree:        ", i.freeram + rbinfree + hugepage_pool_pages);
+	show_val_kb(m, "MemAvailable:   ", available + rbinfree);
 	show_val_kb(m, "Buffers:        ", i.bufferram);
 	show_val_kb(m, "Cached:         ", cached);
 	show_val_kb(m, "SwapCached:     ", total_swapcache_pages());
@@ -115,6 +97,19 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 		    (unsigned long)atomic_long_read(&mmap_pages_allocated));
 #endif
 
+#ifdef CONFIG_ION_RBIN_HEAP
+	show_val_kb(m, "RbinTotal:      ", totalrbin_pages);
+	show_val_kb(m, "RbinAlloced:    ",
+		    atomic_read(&rbin_allocated_pages)
+		    + atomic_read(&rbin_pool_pages));
+	show_val_kb(m, "RbinPool:       ", atomic_read(&rbin_pool_pages));
+	show_val_kb(m, "RbinFree:       ", rbinfree);
+	show_val_kb(m, "RbinCached:     ",
+		    atomic_read(&rbin_cached_pages));
+#endif
+#ifdef CONFIG_KZEROD
+	show_val_kb(m, "ZeroedFree:     ", kzerod_get_zeroed_size());
+#endif
 	show_val_kb(m, "SwapTotal:      ", i.totalswap);
 	show_val_kb(m, "SwapFree:       ", i.freeswap);
 	show_val_kb(m, "Dirty:          ",
@@ -131,10 +126,10 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 	show_val_kb(m, "Slab:           ", sreclaimable + sunreclaim);
 	show_val_kb(m, "SReclaimable:   ", sreclaimable);
 	show_val_kb(m, "SUnreclaim:     ", sunreclaim);
-	show_val_kb2(m, "KernelStack:    %8lu kB\n",
+	seq_printf(m, "KernelStack:    %8lu kB\n",
 		   global_zone_page_state(NR_KERNEL_STACK_KB));
 #ifdef CONFIG_SHADOW_CALL_STACK
-	show_val_kb2(m, "ShadowCallStack:%8lu kB\n",
+	seq_printf(m, "ShadowCallStack:%8lu kB\n",
 		   global_zone_page_state(NR_KERNEL_SCS_BYTES) / 1024);
 #endif
 	show_val_kb(m, "PageTables:     ",
@@ -148,14 +143,14 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 		    global_node_page_state(NR_WRITEBACK_TEMP));
 	show_val_kb(m, "CommitLimit:    ", vm_commit_limit());
 	show_val_kb(m, "Committed_AS:   ", committed);
-	show_val_kb2(m, "VmallocTotal:   %8lu kB\n",
+	seq_printf(m, "VmallocTotal:   %8lu kB\n",
 		   (unsigned long)VMALLOC_TOTAL >> 10);
 	show_val_kb(m, "VmallocUsed:    ", vmalloc_nr_pages());
 	show_val_kb(m, "VmallocChunk:   ", 0ul);
 	show_val_kb(m, "Percpu:         ", pcpu_nr_pages());
 
 #ifdef CONFIG_MEMORY_FAILURE
-	show_val_kb2(m, "HardwareCorrupted: %5lu kB\n",
+	seq_printf(m, "HardwareCorrupted: %5lu kB\n",
 		   atomic_long_read(&num_poisoned_pages) << (PAGE_SHIFT - 10));
 #endif
 
@@ -166,6 +161,9 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 		    global_node_page_state(NR_SHMEM_THPS) * HPAGE_PMD_NR);
 	show_val_kb(m, "ShmemPmdMapped: ",
 		    global_node_page_state(NR_SHMEM_PMDMAPPED) * HPAGE_PMD_NR);
+#ifdef CONFIG_HUGEPAGE_POOL
+	show_val_kb(m, "HugepagePool:   ", hugepage_pool_pages);
+#endif
 	show_val_kb(m, "FileHugePages:  ",
 		    global_node_page_state(NR_FILE_THPS) * HPAGE_PMD_NR);
 	show_val_kb(m, "FilePmdMapped:  ",
@@ -178,20 +176,12 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 		    global_zone_page_state(NR_FREE_CMA_PAGES));
 #endif
 
-	if (m) {
-		hugetlb_report_meminfo(m);
-		arch_report_meminfo(m);
-	}
+	hugetlb_report_meminfo(m);
+
+	arch_report_meminfo(m);
 
 	return 0;
 }
-
-#ifdef CONFIG_QCOM_MINIDUMP_PANIC_DUMP
-void md_dump_meminfo(void)
-{
-	meminfo_proc_show(NULL, NULL);
-}
-#endif
 
 static int __init proc_meminfo_init(void)
 {
